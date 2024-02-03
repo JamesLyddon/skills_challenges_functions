6704b14e6f8f626bee67bf5a13bf9784549485d4
JamesLyddon <jameslyddon@gmail.com>
2024-02-03 13:32:51 +0000

    succefully debugged get_most_common_letter.py

--- lib/cipher.py
+++ lib/cipher.py
@@ -14,15 +14,14 @@ def decode(encrypted, key):
 
     plaintext_chars = []
     for i in encrypted:
-        plain_char = cipher[ord(i) - 65]
-        # plain_char = cipher[65 - ord(i)]
+        plain_char = cipher[ord(i) - 65] # changed from [65 - ord(i)]
         plaintext_chars.append(plain_char)
 
     return "".join(plaintext_chars)
 
 
 def make_cipher(key):
-    alphabet = [chr(i + 97) for i in range(0, 26)]
+    alphabet = [chr(i + 97) for i in range(0, 26)] # changed from alphabet = [chr(i + 98) for i in range(1, 26)]
     cipher_with_duplicates = list(key) + alphabet
 
 
@@ -30,8 +29,6 @@ def make_cipher(key):
     for i in range(0, len(cipher_with_duplicates)):
         if cipher_with_duplicates[i] not in cipher_with_duplicates[:i]:
             cipher.append(cipher_with_duplicates[i])
-
-    print(cipher)
 
     return cipher
 
