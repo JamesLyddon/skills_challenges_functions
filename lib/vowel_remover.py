11de536a4d4ace55dc16a23d0ade37d55ecf0322
JamesLyddon <jameslyddon@gmail.com>
2024-02-05 13:34:31 +0000

    completed vowel_remover.py exercise

---
+++ lib/vowel_remover.py
@@ -0,0 +1,7 @@
+class VowelRemover:
+    def __init__(self, text):
+        self.text = text
+        self.vowels = ["a", "e", "i", "o", "u"]
+
+    def remove_vowels(self):
+        return "".join([char for char in self.text if char.lower() not in self.vowels])
